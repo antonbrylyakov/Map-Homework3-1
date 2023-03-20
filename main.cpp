a9e5ddadbc84b4819d0454ab04aa988716330cc7
Антон Брыляков <yoll@rambler.ru>
2023-03-20 07:29:13 -0700

    Попытка поиска минимума в части массива через разбиение на части

--- main.cpp
+++ main.cpp
@@ -13,20 +13,69 @@ using namespace std::chrono;
 template <typename TIt>
 void selectionSort(TIt begin, TIt end)
 {
-	auto minFunc = [end](TIt localBegin, std::promise<TIt>& minPromise)
+	// с учебной целью возвращать результат будем через future, получаемый из promise
+	auto minFunc = [end](TIt localBegin, TIt localEnd, std::promise<TIt> minPromise)
 	{
 		auto min = std::min_element(localBegin, end);
 		minPromise.set_value(min);
 	};
 
+	
 	while (begin != end)
 	{
-		std::promise<TIt> minPromise;
-		auto minFuture = minPromise.get_future();
-		auto mf = std::async(minFunc, begin, std::ref(minPromise));
-		mf.wait();
-		auto min = minFuture.get();
-		std::swap(*begin, *min);
+		auto size = std::distance(begin, end);
+		auto batchCount =  size < 100 ? 1 : std::thread::hardware_concurrency();
+		auto wholeBatchSize = size / batchCount;
+		auto additionalItems = size % batchCount;
+		auto iterationBegin = begin;
+		auto localBegin = begin;
+		std::vector<std::future<TIt>> futures1;
+		std::vector<std::future<void>> futures2;
+
+		for (size_t i = 0; i < batchCount; ++i)
+		{
+			auto currentBatchSize = wholeBatchSize;
+			if (additionalItems > 0)
+			{
+				++currentBatchSize;
+				--additionalItems;
+			}
+
+			auto localEnd = localBegin;
+			std::advance(localEnd, currentBatchSize);
+
+			std::promise<TIt> minPromise;
+			futures1.push_back(minPromise.get_future());
+
+			if (i == batchCount - 1)
+			{
+				minFunc(localBegin, localEnd, std::move(minPromise));
+			}
+			else
+			{
+				futures2.push_back(std::async(minFunc, localBegin, localEnd, std::move(minPromise)));
+			}
+
+			localBegin = localEnd;
+		}
+
+		for (auto& f : futures2)
+		{
+			f.wait();
+		}
+
+		std::optional<TIt> min;
+
+		for (auto& f : futures1)
+		{
+			auto currIt = f.get();
+			if (!min.has_value() || *min.value() > *currIt)
+			{
+				min = currIt;
+			}
+		}
+
+		std::swap(*min.value(), *iterationBegin);
 		++begin;
 	}
 }
